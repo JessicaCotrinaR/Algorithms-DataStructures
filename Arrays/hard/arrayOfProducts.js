092eea512c4bbc2adb770766985abf2ae2c1c1b9
Jessica <jessicacotrina@gmail.com>
2021-01-25 10:56:12 -0500

    array of products

---
+++ Arrays/hard/arrayOfProducts.js
@@ -0,0 +1,21 @@
+function arrayOfProducts(array) {
+  // Write your code here.
+  const products = new Array(array.length).fill(1);
+  //[1,1,1,1]
+  let leftRunningProduct = 1;
+  for (let i = 0; i < array.length; i++) {
+    products[i] = leftRunningProduct;
+    console.log("left", products[i]);
+    leftRunningProduct *= array[i];
+    console.log("runleft", leftRunningProduct);
+  }
+  let rigthRunningProduct = 1;
+  for (let i = array.length - 1; i > -1; i--) {
+    products[i] *= rigthRunningProduct;
+    console.log("right", products[i]);
+    rigthRunningProduct *= array[i];
+    console.log("runrigth", rigthRunningProduct);
+  }
+  return products;
+}
+console.log(arrayOfProducts([5, 1, 4, 2]));
