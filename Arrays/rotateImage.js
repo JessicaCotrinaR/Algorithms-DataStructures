d827c7f32fb04c887f2ed13ff59d4f15d18a3975
Jessica <jessicacotrina@gmail.com>
2021-05-04 16:50:26 -0400

    rotate Image

---
+++ Arrays/rotateImage.js
@@ -0,0 +1,34 @@
+function rotateImage(matrix) {
+  let n = matrix.length;
+  //start traversing
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      //swap
+      let tmp = matrix[i][j]; // have a temp
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = tmp;
+      if (i == j) {
+        break;
+      }
+    }
+  }
+
+  for (let k = 0; k < n; k++) {
+    matrix[k].reverse();
+  }
+  return matrix;
+}
+let matrix = [
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
+  [13, 3, 6, 7],
+  [15, 14, 12, 16],
+];
+/*
+    .
+[[1,2,3],
+   .
+  [4,5,6],
+  [7,8,9]]
+*/
+console.log(rotateImage(matrix));
