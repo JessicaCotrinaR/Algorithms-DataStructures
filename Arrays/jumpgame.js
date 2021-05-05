a5ce2f7538d324ea87b7f86f64f2d7e701944842
Jessica <jessicacotrina@gmail.com>
2021-05-04 22:31:32 -0400

    jump game

--- Arrays/jumpgame.js
+++ Arrays/jumpgame.js
@@ -5,3 +5,20 @@ Each element in the array represents your maximum jump length at that position.
 
 Determine if you are able to reach the last index.
 */
+function canJump(nums) {
+  if (nums && nums.length <= 1) {
+    return true;
+  }
+  let curMax = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (i > curMax) {
+      return false;
+    }
+
+    if (curMax >= nums.length - 1) {
+      return true;
+    }
+
+    curMax = Math.max(nums[i] + i, curMax);
+  }
+}
